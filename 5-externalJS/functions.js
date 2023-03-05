7276729c392cd6a7f088db4a56e5b8a4bca07b6b
BrajewskiBartosz <brajewskibartosz@gmail.com>
2023-03-05 10:06:44 +0100

    initial commit

---
+++ 5-externalJS/functions.js
@@ -0,0 +1,4 @@
+function setBaseFontStyle() {
+    document.getElementById("pragma").innerHTML = "";
+    document.getElementById("pragma").style.fontSize = "16px"
+}
---
+++ 6-simpleCalculation/functions.js
@@ -0,0 +1,8 @@
+function simpleCalculation() {
+
+    let a, b, c;
+    a = 5
+    b = 6
+    c = 5 + 6;
+    document.getElementById("calculatedValue").innerHTML = c;
+}
